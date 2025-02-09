e9394280d12026937f63031e3d8e167e5287f952
Mingzhuo Yin <yinmingzhuo@gmail.com>
2025-02-09 11:59:46 +0800

    chore: change `segment_growing_max_page_size` guc default value (#40)
    Signed-off-by: Mingzhuo Yin <yinmingzhuo@gmail.com>

--- src/guc.rs
+++ src/guc.rs
@@ -2,7 +2,7 @@ use pgrx::{GucContext, GucFlags, GucRegistry, GucSetting};
 
 pub static BM25_LIMIT: GucSetting<i32> = GucSetting::<i32>::new(100);
 pub static ENABLE_INDEX: GucSetting<bool> = GucSetting::<bool>::new(true);
-pub static SEGMENT_GROWING_MAX_PAGE_SIZE: GucSetting<i32> = GucSetting::<i32>::new(1000);
+pub static SEGMENT_GROWING_MAX_PAGE_SIZE: GucSetting<i32> = GucSetting::<i32>::new(4096);
 
 pub unsafe fn init() {
     GucRegistry::define_int_guc(
